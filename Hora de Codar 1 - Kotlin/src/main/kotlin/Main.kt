64e4d16e9201941869d54bbbbd5aca80f53e3a50
devcarolvieira <ana.cavieira4@senacsp.edu.br>
2026-03-18 17:20:46 -0300

    Add files via upload

---
+++ Hora de Codar 1 - Kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    // Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
+
+    val nomeDoCarro = "fusca"
+
+    println("$nomeDoCarro")
+
+}
